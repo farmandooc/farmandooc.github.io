bd6ed3de214cb44088fdba208b1666d0f9b9f86c
Osniel <osniel@casa>
2025-09-06 13:34:46 -0400

    Auto update - 2025-09-06 13:34:46

--- script_update.cmd
+++ script_update.cmd
@@ -4,7 +4,7 @@ set fechaHora=%date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,8%
 echo Fecha y hora: %fechaHora%
 REM Renombrar archivo de imagen
 set /a contador=%random% %% 1000
-for %%f in ("Use_normal copy*.jpg") do ren "%%f" "Use_normal copy%contador%.jpg"
+REM for %%f in ("Use_normal copy*.jpg") do ren "%%f" "Use_normal copy%contador%.jpg"
 git add .
 git commit -m "Auto update - %fechaHora%"
 git push -u origin master
