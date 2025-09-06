63d5841ac988b858f197b61c46928268480ccf42
Osniel <osniel@casa>
2025-09-06 19:25:06 -0400

    Auto update - 2025-09-06 19:25:06

--- files.js
+++ files.js
@@ -2,8 +2,12 @@
 const files = [ 
     'Admin_user.jpg', 
     'avatar_logo.png', 
+    'icons8-heart-100.png', 
+    'icons8-heart-50.png', 
     'index.html', 
     'osniel-Avatar.jpg', 
-    'osniel.jpg', 
+    'Sin t¡tulo.png', 
+    'star-filled_yellow.png', 
+    'star.png', 
     'Use_normal.jpg', 
 ]; 
